d128f8e40a72839c0e64793aec7d0144248f6945
Grayyy1219 <lance.musngi@gamil.com>
2024-07-19 00:45:13 +0800

    db

--- config/ric_events.sql
+++ config/ric_events.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2024 at 10:54 AM
+-- Generation Time: Jul 18, 2024 at 06:43 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,6 +29,7 @@ USE `ric_events`;
 -- Table structure for table `category`
 --
 
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `CategoryID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -39,6 +40,7 @@ CREATE TABLE `category` (
 -- Table structure for table `currentuser`
 --
 
+DROP TABLE IF EXISTS `currentuser`;
 CREATE TABLE `currentuser` (
   `UserId` int(11) NOT NULL,
   `FName` varchar(50) NOT NULL,
@@ -55,7 +57,7 @@ CREATE TABLE `currentuser` (
 --
 
 INSERT INTO `currentuser` (`UserId`, `FName`, `username`, `email`, `address`, `phone`, `profile`, `admin`) VALUES
-(1, 'ric mar', '0', 'johnricmar.alagos.c@gmail.com', NULL, NULL, 'uploads/profile/bf098655-3eb7-408e-b7d5-e4a3e1a1bd79.jpg', 0);
+(1, 'Lance Musngi', 'lance', 'lance.musngi@gmail.com', NULL, NULL, 'css/img/new.png', 0);
 
 -- --------------------------------------------------------
 
@@ -63,32 +65,64 @@ INSERT INTO `currentuser` (`UserId`, `FName`, `username`, `email`, `address`, `p
 -- Table structure for table `events`
 --
 
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
   `EventID` int(11) NOT NULL,
   `EventTitle` varchar(255) NOT NULL,
   `Description` varchar(1000) NOT NULL,
   `Date` date NOT NULL,
   `Location` varchar(255) NOT NULL,
-  `Available` int(11) NOT NULL
+  `EventImg` varchar(255) NOT NULL,
+  `Available` int(11) NOT NULL,
+  `status` int(11) NOT NULL COMMENT '0 = need aproval\r\n1 = aproved',
+  `UserID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`EventID`, `EventTitle`, `Description`, `Date`, `Location`, `Available`) VALUES
-(1, 'Career Fair', 'Meet potential employers and explore job opportunities. Open to all graduating students.', '2024-05-22', 'Bulacan State University Gymnasium', 0),
-(2, 'Science Symposium', 'Presentations and discussions on recent advancements in various fields of science.', '2024-06-06', 'Bulacan State University Gymnasium', 1),
-(3, 'Sleep Over ', 'Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis ', '2024-05-31', 'Adrian\'s House', 1),
-(4, 'Coding Bootcamp', 'Intensive coding bootcamp covering Python, JavaScript, and SQL. Ideal for beginners and intermediate coders.\r\n', '2024-06-02', 'Bulacan State University', 0),
-(5, 'Art Exhibit', 'Showcase of student artworks from the Fine Arts department. Open to the public.\r\n', '2024-06-06', 'Bulacan State University Gymnasium', 0),
-(6, 'Music Festival', 'Annual music festival featuring bands and solo artists from the university.\r\n', '2024-06-04', 'Bulacan State University Main Grounds', 0),
-(7, 'Tech Startup Workshop', 'Workshop for aspiring tech entrepreneurs covering startup essentials and pitching techniques.', '2024-06-17', 'Innovate Hub Co-working Space', 0),
-(8, 'Fashion Show', 'Annual fashion show featuring designs by students and local designers. Runway and exhibition.', '2024-06-18', 'Glamour Events Hall', 0),
-(9, 'Food Festival', 'Celebration of local cuisine with food stalls, cooking demonstrations, and live music.', '2024-06-19', 'Culinary Delights Plaza', 0),
-(10, 'Fitness Bootcamp', 'Intensive fitness bootcamp with certified trainers. Outdoor workout sessions and nutrition seminars.', '2024-06-20', 'FitZone Park', 0),
-(11, 'Artisan Market', 'Market showcasing handmade crafts, arts, and gourmet foods by local artisans. Shopping and live entertainment.', '2024-06-21', 'Artisan Alley', 0),
-(18, 'celeb', 'graduation', '2024-06-04', '8waves', 0);
+INSERT INTO `events` (`EventID`, `EventTitle`, `Description`, `Date`, `Location`, `EventImg`, `Available`, `status`, `UserID`) VALUES
+(1, 'Career Fair', 'Meet potential employers and explore job opportunities. Open to all graduating students.', '2024-07-23', 'Bulacan State University Gymnasium', 'uploads\\events\\Bulacan State University Gymnasium.jpg', 200, 1, 1),
+(2, 'Science Symposium', 'Presentations and discussions on recent advancements in various fields of science.', '2024-07-24', 'Bulacan State University Gymnasium', 'uploads\\events\\Bulacan State University Gymnasium.jpg', 200, 1, 1),
+(3, 'Sleep Over ', 'Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis Sleep Over sa bahay ni James Raille Sition, Foods dala ni Mae Reyes then si adrian Tokis ', '2024-07-18', 'Adrian\'s House', 'uploads\\events\\sleepover.png', 30, 1, 1),
+(4, 'Coding Bootcamp', 'Intensive coding bootcamp covering Python, JavaScript, and SQL. Ideal for beginners and intermediate coders.\r\n', '2024-07-25', 'Bulacan State University', 'uploads\\events\\Bulacan State University.jpg', 50, 1, 1),
+(5, 'Art Exhibit', 'Showcase of student artworks from the Fine Arts department. Open to the public.\r\n', '2024-07-26', 'Bulacan State University Gymnasium', 'uploads\\events\\Bulacan State University Gymnasium.jpg', 50, 1, 1),
+(6, 'Music Festival', 'Annual music festival featuring bands and solo artists from the university.\r\n', '2024-07-28', 'Bulacan State University Main Grounds', 'uploads\\events\\Bulacan State University Main Grounds.jpg', 50, 1, 1),
+(7, 'Tech Startup Workshop', 'Workshop for aspiring tech entrepreneurs covering startup essentials and pitching techniques.', '2024-07-29', 'Innovate Hub Co-working Space', 'uploads\\events\\Innovate Hub Co-working Space.jpg', 10, 1, 1),
+(8, 'Fashion Show', 'Annual fashion show featuring designs by students and local designers. Runway and exhibition.', '2024-07-30', 'Glamour Events Hall', 'uploads\\events\\Glamour Events Hall.jpg', 50, 1, 1),
+(9, 'Food Festival', 'Celebration of local cuisine with food stalls, cooking demonstrations, and live music.', '2024-07-31', 'Culinary Delights Plaza', 'uploads\\events\\Culinary Delights Plaza.jpg', 1, 1, 1),
+(10, 'Fitness Bootcamp', 'Intensive fitness bootcamp with certified trainers. Outdoor workout sessions and nutrition seminars.', '2024-07-20', 'FitZone Park', 'uploads\\events\\fitzone.jpg', 100, 1, 1),
+(11, 'Artisan Market', 'Market showcasing handmade crafts, arts, and gourmet foods by local artisans. Shopping and live entertainment.', '2024-07-21', 'Artisan Alley', 'uploads\\events\\Artisan Alley.jpg', 50, 1, 15),
+(43, 'q', 'q', '2024-07-01', 'Adrian', 'uploads/events/5d05498f-d3e8-4f3d-80af-f80070ec70d9.jpg', 50, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location` (
+  `LocationID` int(11) NOT NULL,
+  `LocationName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`LocationID`, `LocationName`) VALUES
+(2, 'Adrian\'s House'),
+(3, 'Artisan Alley'),
+(4, 'Bulacan State University'),
+(1, 'Bulacan State University Gymnasium'),
+(5, 'Bulacan State University Main Grounds'),
+(6, 'Culinary Delights Plaza'),
+(7, 'FitZone Park'),
+(8, 'Glamour Events Hall'),
+(9, 'Innovate Hub Co-working Space'),
+(10, 'test');
 
 -- --------------------------------------------------------
 
@@ -96,6 +130,7 @@ INSERT INTO `events` (`EventID`, `EventTitle`, `Description`, `Date`, `Location`
 -- Table structure for table `myevents`
 --
 
+DROP TABLE IF EXISTS `myevents`;
 CREATE TABLE `myevents` (
   `MyEventID` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
@@ -108,10 +143,11 @@ CREATE TABLE `myevents` (
 --
 
 INSERT INTO `myevents` (`MyEventID`, `customer_id`, `eventid`, `status`) VALUES
-(4, 11, 3, 1),
-(19, 12, 4, 1),
-(20, 13, 2, 1),
-(22, 14, 4, 1);
+(27, 1, 3, 0),
+(28, 1, 9, 0),
+(41, 15, 3, 0),
+(44, 1, 29, 0),
+(45, 1, 10, 0);
 
 -- --------------------------------------------------------
 
@@ -119,6 +155,7 @@ INSERT INTO `myevents` (`MyEventID`, `customer_id`, `eventid`, `status`) VALUES
 -- Table structure for table `page`
 --
 
+DROP TABLE IF EXISTS `page`;
 CREATE TABLE `page` (
   `ItemID` int(11) NOT NULL,
   `Itemname` varchar(50) NOT NULL,
@@ -142,6 +179,7 @@ INSERT INTO `page` (`ItemID`, `Itemname`, `value`) VALUES
 -- Table structure for table `slideshow`
 --
 
+DROP TABLE IF EXISTS `slideshow`;
 CREATE TABLE `slideshow` (
   `SlideID` int(11) NOT NULL,
   `imagename` varchar(50) NOT NULL,
@@ -169,6 +207,7 @@ INSERT INTO `slideshow` (`SlideID`, `imagename`, `imagelocation`) VALUES
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `UserID` int(11) NOT NULL,
   `FName` varchar(255) NOT NULL,
@@ -188,9 +227,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`UserID`, `FName`, `Username`, `Password`, `Email`, `profile`, `verification_code`, `verified`, `admin`) VALUES
 (1, 'Ric Eventss', 'admin', '$2y$10$oilozmptugxLRFN2w/DuCe3jwfVC5P9tGvPBFj5ShPX.bffpErUW.', 'ric.events@gmail.com', 'uploads/profile/logo.png', 1, 1, 1),
 (11, 'Leoniel Mae Reyes', 'Leo', '$2y$10$.YYROuWgaxYv.Uo0Lxffx.lHu9yq7.Rp9bMGnUKpFwlt/UhJErNaC', 'reyesleoneilmae@gmail.com', 'css/img/new.png', 0, 0, 0),
-(12, 'Jaja Madrideo', 'rai', '$2y$10$bVN0D3oWYYcZXV5iISjOT.yBI0O76lXaPP4AgFv6C7JYXSfZDjs9m', 'Jaja Madrideo@gmail.com', 'uploads/profile/rai.jpg', 22584, 1, 0),
 (13, 'jan ric', 'ric', '$2y$10$c.hM2Nd3sd28zLXaqFDEdOTwDoK36q8blkkm9ZNfrkhlNsFMxcKZa', 'johnricmaralagos9@gmail.com', 'css/img/new.png', 16844, 1, 0),
-(14, 'ric mar', 'ricmar', '$2y$10$7ARA6p7Qwof61.E91gZ/7.z9aAyMMeRC0/DayMabRdpMLI9s19.6m', 'johnricmar.alagos.c@gmail.com', 'uploads/profile/bf098655-3eb7-408e-b7d5-e4a3e1a1bd79.jpg', 55439, 1, 1);
+(14, 'ric mar', 'ricmar', '$2y$10$7ARA6p7Qwof61.E91gZ/7.z9aAyMMeRC0/DayMabRdpMLI9s19.6m', 'johnricmar.alagos.c@gmail.com', 'uploads/profile/bf098655-3eb7-408e-b7d5-e4a3e1a1bd79.jpg', 55439, 1, 1),
+(15, 'Lance Musngi', 'lance', '$2y$10$qZ9z3..HAEChN6Xd5sXjju.3VMznwztaokjF4IgpnDKz2O1sAbKCG', 'lance.musngi@gmail.com', 'css/img/new.png', 19800, 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -212,7 +251,15 @@ ALTER TABLE `currentuser`
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
-  ADD PRIMARY KEY (`EventID`);
+  ADD PRIMARY KEY (`EventID`),
+  ADD UNIQUE KEY `Date` (`Date`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`LocationID`),
+  ADD UNIQUE KEY `LocationName` (`LocationName`);
 
 --
 -- Indexes for table `myevents`
@@ -258,13 +305,19 @@ ALTER TABLE `currentuser`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `EventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `EventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `LocationID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `myevents`
 --
 ALTER TABLE `myevents`
-  MODIFY `MyEventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `MyEventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `page`
@@ -282,7 +335,7 @@ ALTER TABLE `slideshow`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
